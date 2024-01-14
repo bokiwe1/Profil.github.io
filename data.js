3800b6ad114e114d5c006939f911bf9773223ce4
mhmdfiqrif <b0b0b03y@gmail.com>
2024-01-14 18:19:45 +0700

    profil

---
+++ data.js
@@ -0,0 +1,7 @@
+function redirInstagram () {
+    window.location.href = "https://instagram.com/mhmdfiqrifzi"
+}
+
+function redirWhatsapp () {
+    window.location.href = "https://wa.me/+6283840651947"
+}
